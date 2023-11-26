da4ad824955758e909a60fed5950ef6c52e49bc6
vishnuskulkarni <53376359+vishnuskulkarni@users.noreply.github.com>
2023-11-26 14:52:58 -0500

    fixed bug

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -41,7 +41,7 @@ describe('renders the app', () => {
   });
 
   it('should render the title', async () => {
-    expect(document.title).toBe("Vishnu Kulkarni");
+    expect(document.title).toBe('Vishnu Kulkarni');
   });
 
   it('can navigate to /about', async () => {
